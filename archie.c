ba0beb4e43b021b1b8eec419ae14535e159c5d3d
Gurov <103680464+Gur0v@users.noreply.github.com>
2024-12-08 16:26:12 +0200

    Update archie.c

--- archie.c
+++ archie.c
@@ -447,7 +447,7 @@ int main(int argc, char *argv[]) {
 
     while (1) {
         char input_line[MAX_INPUT_LENGTH];
-        get_input(input_line, "$> ");
+        get_input(input_line, "$ ");
         if (*input_line) {
             add_history(input_line);
             handle_command(input_line, package_manager);
